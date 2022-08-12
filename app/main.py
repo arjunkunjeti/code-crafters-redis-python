a30f10370f243d8478c5e760758f8ebfbae8f9eb
Edwin Zamudio <ezamudio@hawk.iit.edu>
2022-08-12 18:41:29 -0500

    pong3

--- app/main.py
+++ app/main.py
@@ -10,9 +10,21 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    client, _ = server_socket.accept()
-    client.recv(1024)
-    client.send(b"+PONG\r\n")
+    # while there are still clients connected to the server
+    while True:
+        # accept a new client connection
+        client, _ = server_socket.accept()
+
+        # receive data from the client
+        data = client.recv(1024)
+
+        # if the data is not empty
+        if data:
+            # send data back to the client
+            client.send(b"+PONG\r\n")
+
+        # close the client connection
+        client.close()
 
 if __name__ == "__main__":
     main()
